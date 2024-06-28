0cbe869cadfce2b88fdfcc84ec870ee3c5cdcdca
MARINA MARIA LUCENTINI <158228930+MarinaLucentini@users.noreply.github.com>
2024-06-28 11:11:10 +0200

    configuration Progect

---
+++ src/main/java/marinalucentini/backend_esame_w6/BackendEsameW6Application.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_esame_w6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEsameW6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEsameW6Application.class, args);
+	}
+
+}
---
+++ src/test/java/marinalucentini/backend_esame_w6/BackendEsameW6ApplicationTests.java
@@ -0,0 +1,13 @@
+package marinalucentini.backend_esame_w6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEsameW6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
